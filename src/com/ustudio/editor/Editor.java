ec36b56063d751b41f8130a5fbf23088d6db02fd
ionini <ioni.niela@gmail.com>
2012-09-24 18:32:07 -0300

    El editor ya se puede agregar notas con la clase Key.

--- src/com/ustudio/editor/Editor.java
+++ src/com/ustudio/editor/Editor.java
@@ -1,5 +1,86 @@
 package com.ustudio.editor;
 
-public class Editor {
+import org.anddev.andengine.entity.Entity;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.input.touch.TouchEvent;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
+import android.view.MotionEvent;
+
+import com.ustudio.main.MainActivity;
+
+public class Editor extends Entity{
+	
+	private float entityWidth;
+	private float entityHight;
+	
+	private Texture mTexture;
+	private TextureRegion backTexture;
+	private TextureRegion keyTexture;
+	private Entity all;
+	
+	private float mTouchX = 0, mTouchY = 0, mTouchOffsetX = 0, mTouchOffsetY = 0;
+	private Sprite scroll;
+	
+	
+	public Editor(float w, float h, Scene pScene){
+		entityHight = h;
+		entityWidth = w;
+		
+		this.mTexture = new BitmapTextureAtlas(256, 1024, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.backTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset((BitmapTextureAtlas) this.mTexture, MainActivity.getInstance().getApplicationContext(), "gfx/Teclas/Blancas/BN.png", 0, 0);
+		this.keyTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset((BitmapTextureAtlas) this.mTexture, MainActivity.getInstance().getApplicationContext(), "gfx/Teclas/Negras/NN.png", 99, 0);
+		MainActivity.getInstance().getEngine().getTextureManager().loadTexture(this.mTexture);
+		
+		all = new Entity();
+		
+		scroll = new Sprite(0 ,0 ,this.backTexture){
+			public boolean onAreaTouched(final TouchEvent pTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+				if(pTouchEvent.getAction() == MotionEvent.ACTION_DOWN)
+		        {
+		                mTouchX = pTouchEvent.getMotionEvent().getX();
+		                mTouchY = pTouchEvent.getMotionEvent().getY();
+		        }
+		        else if(pTouchEvent.getAction() == MotionEvent.ACTION_MOVE)
+		        {
+		                float newX = pTouchEvent.getMotionEvent().getX();
+		                float newY = pTouchEvent.getMotionEvent().getY();
+		               
+		                mTouchOffsetX = (newX - mTouchX);
+		                mTouchOffsetY = (newY - mTouchY);
+		               
+		                float newScrollX = (float) (all.getX() + mTouchOffsetX*0.5);
+		                float newScrollY = all.getY() + mTouchOffsetY*2;
+		               
+		                all.setPosition(newScrollX, newScrollY);
+		               
+		                mTouchX = newX;
+		                mTouchY = newY;
+		        }
+			
+				return true;
+			}
+		};
+		scroll.setWidth(1000);
+		scroll.setHeight(1000);
+		
+		all.attachChild(scroll);
+		pScene.registerTouchArea(scroll);
+		
+		addNote(1,2,43,200,1);
+		
+		this.attachChild(all); //No tocar.
+	}
+	
+	public void addNote(int id, int note, float tempo, float lenght, float volume){
+		Key k = new Key(id, note, tempo, lenght, volume);
+		all.attachChild(k);
+		
+	}
+	
 }
--- src/com/ustudio/scenes/PutoScene.java
+++ src/com/ustudio/scenes/PutoScene.java
@@ -3,67 +3,32 @@ package com.ustudio.scenes;
 
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
-import org.anddev.andengine.entity.sprite.Sprite;
-import org.anddev.andengine.input.touch.TouchEvent;
-import org.anddev.andengine.opengl.texture.Texture;
-import org.anddev.andengine.opengl.texture.TextureOptions;
-import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
-import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
-import org.anddev.andengine.opengl.texture.region.TextureRegion;
 
-import com.ustudio.main.MainActivity;
+import com.ustudio.editor.Editor;
 
 public class PutoScene extends Scene {
 
 	static int CAMERA_WIDTH ;
 	static int CAMERA_HEIGHT ;
 
-	private Texture mTexture;
-	private TextureRegion mFaceTextureRegion;
-	private Sprite back;
+
+	private Editor mEditor;
 	
 	
 	
 	public PutoScene(int w, int h) {
+		
+		
+		this.mEditor = new Editor(0, 0, this);
+		this.attachChild(this.mEditor);
 
 		CAMERA_WIDTH = w;
 		CAMERA_HEIGHT = h;
 		//this.mMainActivity = pMainActivity;
         this.setBackground(new ColorBackground(0.8784f, 0, 0));
         
-        this.mTexture = new BitmapTextureAtlas(512, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-		this.mFaceTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset((BitmapTextureAtlas) this.mTexture, MainActivity.getInstance().getApplicationContext(), "gfx/Botones/R-U R-D.png", 0, 0);
-		MainActivity.getInstance().getEngine().getTextureManager().loadTexture(this.mTexture);
-		
-		final int centerX = (10);
-        final int centerY = (10);
-       
-        back = new Sprite(centerX, centerY, this.mFaceTextureRegion){
-        	@Override
-            public boolean onAreaTouched(final TouchEvent pAreaTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
-                switch(pAreaTouchEvent.getAction()) {
-                    case TouchEvent.ACTION_DOWN:                    
-                        this.setScale(1.25f);
-                        this.setAlpha(0.5f);
-                        break;
-                    case TouchEvent.ACTION_UP:  
-                        this.setScale(1.0f);
-                        this.setAlpha(1.0f);
-                        menuItemPressed(1);
-                        break;
-                        }
-                
-                        return true;
-            }
-        };
-        back.setWidth(780);
-        back.setHeight(75);
-        this.attachChild(back);
-        this.registerTouchArea(back);
-        
 	}
-	public void menuItemPressed(int id){
-		
-		MainActivity.getInstance().getSceneManager().setMenuScene(CAMERA_WIDTH, CAMERA_HEIGHT);
-	}
+	
+	
+	
 }
